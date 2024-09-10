8cd6db195488c92d0b48e3d283fda7bc9121b60c
ST10082700 <st10082700@vcconnect.edu.za>
2024-09-10 22:50:01 +0200

    Initial commit with Dockerfile and project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG6212___CMCS___ST10082700.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

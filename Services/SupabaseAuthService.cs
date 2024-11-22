14c17ee5baa93b6e77c0fb9d33bd5ef10d9bbed7
ST10082700 <st10082700@vcconnect.edu.za>
2024-11-22 22:13:46 +0200

    Adding Supabase Auth instead Indentity bc of my db choice

---
+++ Services/SupabaseAuthService.cs
@@ -0,0 +1,41 @@
+﻿using Supabase.Gotrue;
+using Supabase;
+
+namespace PROG6212___CMCS___ST10082700.Services
+{
+    public class SupabaseAuthService
+    {
+        private readonly Supabase.Client _supabaseClient;
+
+        public SupabaseAuthService(IConfiguration configuration)
+        {
+            var options = new SupabaseOptions
+            {
+                AutoRefreshToken = true,
+                AutoConnectRealtime = true
+            };
+
+            _supabaseClient = new Supabase.Client(
+                configuration["Supabase:Url"],
+                configuration["Supabase:Key"],
+                options
+            );
+        }
+
+        public async Task<Session> SignIn(string email, string password)
+        {
+            var response = await _supabaseClient.Auth.SignIn(email, password);
+            return response;
+        }
+
+        public async Task SignOut()
+        {
+            await _supabaseClient.Auth.SignOut();
+        }
+
+        public async Task<User> GetUser()
+        {
+            return await _supabaseClient.Auth.GetUser();
+        }
+    }
+}
---
+++ Services/SupabaseAuthenticationMiddleware.cs
@@ -0,0 +1,6 @@
+﻿namespace PROG6212___CMCS___ST10082700.Services
+{
+    public class SupabaseAuthenticationMiddleware
+    {
+    }
+}
